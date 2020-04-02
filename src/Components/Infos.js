3497d12cdd1b04383fedafa701be9ae9b3ad7261
JA21 <jorgeandres2104@gmail.com>
2020-04-01 21:55:45 -0500

    estilos con js

--- src/Components/Infos.js
+++ src/Components/Infos.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-
+import './info.css';
 class Infos extends Component {
     render() {
-        return <div>
-            <h1>Nombre : {this.props.tarea.Nombre}</h1>
-            <h2>Rango: {this.props.tarea.rango}</h2>
-            <p>{this.props.tarea.descripcion}</p>
+
+        const {tarea}=this.props;
+        //const redcolor = {Background: 'red'}
+        return <div >
+            <h1>Nombre : {tarea.Nombre}</h1>
+            <h2>Rango: {tarea.rango}</h2>
+            <p>{tarea.descripcion}</p>
             <input type="Checkbox" />
-            <button> x</button>
+            <button style={btnestilo}> x</button>
         </div>
 
     }
+}
+
+const btnestilo={
+    fontSize:'18px',
+    Background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '13px 15px',
+    borderradius: '30%',
+    float:'rigth',
+    cursor:'pointer'
 }
 
 export default Infos;
